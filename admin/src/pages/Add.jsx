a92c46a1d3ee986d27bdbe7f33f23ceb0d291801
nethminisuraji-hewage <nethminisuraji@gmail.com>
2025-03-03 12:00:00 +0530

    admin add products page

--- admin/src/pages/Add.jsx
+++ admin/src/pages/Add.jsx
@@ -6,60 +6,7 @@ import { toast } from 'react-toastify'
 
 const Add = ({token}) => {
 
-  const [image1,setImage1] = useState(false)
-  const [image2,setImage2] = useState(false)
-  const [image3,setImage3] = useState(false)
-  const [image4,setImage4] = useState(false)
-
-   const [name, setName] = useState("");
-   const [description, setDescription] = useState("");
-   const [price, setPrice] = useState("");
-   const [category, setCategory] = useState("Cricket");
-   const [subCategory, setSubCategory] = useState("Bats");
-   const [bestseller, setBestseller] = useState(false);
-   const [sizes, setSizes] = useState(["Standard"]);
-
-   const onSubmitHandler = async (e) => {
-    e.preventDefault();
-
-    try {
-      
-      const formData = new FormData()
-
-      formData.append("name",name)
-      formData.append("description",description)
-      formData.append("price",price)
-      formData.append("category",category)
-      formData.append("subCategory",subCategory)
-      formData.append("bestseller",bestseller)
-      formData.append("sizes",JSON.stringify(sizes))
-
-      image1 && formData.append("image1",image1)
-      image2 && formData.append("image2",image2)
-      image3 && formData.append("image3",image3)
-      image4 && formData.append("image4",image4)
-
-      const response = await axios.post(backendUrl + "/api/product/add",formData,{headers:{token}})
-
-      if (response.data.success) {
-        toast.success(response.data.message)
-        setName('')
-        setDescription('')
-        setImage1(false)
-        setImage2(false)
-        setImage3(false)
-        setImage4(false)
-        setPrice('')
-      } else {
-        toast.error(response.data.message)
-      }
-
-    } catch (error) {
-      console.log(error);
-      toast.error(error.message)
-    }
-   }
-
+  
   return (
     <form onSubmit={onSubmitHandler} className='flex flex-col w-full items-start gap-3'>
         <div>
