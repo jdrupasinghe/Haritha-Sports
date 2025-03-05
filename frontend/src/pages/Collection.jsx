bc7777e2862a0d61cf73b041539873f7e21e7a0b
Thimali1111 <thimalikaushalya1998@gmail.com>
2025-03-05 12:00:00 +0530

    products UI functions

--- frontend/src/pages/Collection.jsx
+++ frontend/src/pages/Collection.jsx
@@ -6,7 +6,81 @@ import ProductItem from '../components/ProductItem';
 
 const Collection = () => {
 
-  
+  const { products , search , showSearch } = useContext(ShopContext);
+  const [showFilter,setShowFilter] = useState(false);
+  const [filterProducts,setFilterProducts] = useState([]);
+  const [category,setCategory] = useState([]);
+  const [subCategory,setSubCategory] = useState([]);
+  const [sortType,setSortType] = useState('relavent')
+
+  const toggleCategory = (e) => {
+
+    if (category.includes(e.target.value)) {
+        setCategory(prev=> prev.filter(item => item !== e.target.value))
+    }
+    else{
+      setCategory(prev => [...prev,e.target.value])
+    }
+
+  }
+
+  const toggleSubCategory = (e) => {
+
+    if (subCategory.includes(e.target.value)) {
+      setSubCategory(prev=> prev.filter(item => item !== e.target.value))
+    }
+    else{
+      setSubCategory(prev => [...prev,e.target.value])
+    }
+  }
+
+  const applyFilter = () => {
+
+    let productsCopy = products.slice();
+
+    if (showSearch && search) {
+      productsCopy = productsCopy.filter(item => item.name.toLowerCase().includes(search.toLowerCase()))
+    }
+
+    if (category.length > 0) {
+      productsCopy = productsCopy.filter(item => category.includes(item.category));
+    }
+
+    if (subCategory.length > 0 ) {
+      productsCopy = productsCopy.filter(item => subCategory.includes(item.subCategory))
+    }
+
+    setFilterProducts(productsCopy)
+
+  }
+
+  const sortProduct = () => {
+
+    let fpCopy = filterProducts.slice();
+
+    switch (sortType) {
+      case 'low-high':
+        setFilterProducts(fpCopy.sort((a,b)=>(a.price - b.price)));
+        break;
+
+      case 'high-low':
+        setFilterProducts(fpCopy.sort((a,b)=>(b.price - a.price)));
+        break;
+
+      default:
+        applyFilter();
+        break;
+    }
+
+  }
+
+  useEffect(()=>{
+      applyFilter();
+  },[category,subCategory,search,showSearch,products])
+
+  useEffect(()=>{
+    sortProduct();
+  },[sortType])
 
   return (
     <div className='flex flex-col sm:flex-row gap-1 sm:gap-10 pt-10 border-t'>
