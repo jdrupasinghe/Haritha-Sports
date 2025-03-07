d3fa7d13a8f4e77c021a6d541a5ce97ef29b8da2
Shalini64 <shalinikarthikeyan27@gmail.com>
2025-03-07 12:00:00 +0530

    admin login ui

--- admin/src/components/Login.jsx
+++ admin/src/components/Login.jsx
@@ -5,24 +5,7 @@ import { toast } from 'react-toastify'
 
 const Login = ({setToken}) => {
 
-    const [email,setEmail] = useState('')
-    const [password,setPassword] = useState('')
-
-    const onSubmitHandler = async (e) => {
-        try {
-            e.preventDefault();
-            const response = await axios.post(backendUrl + '/api/user/admin',{email,password})
-            if (response.data.success) {
-                setToken(response.data.token)
-            } else {
-                toast.error(response.data.message)
-            }
-             
-        } catch (error) {
-            console.log(error);
-            toast.error(error.message)
-        }
-    }
+    
 
   return (
     <div className='min-h-screen flex items-center justify-center w-full'>
