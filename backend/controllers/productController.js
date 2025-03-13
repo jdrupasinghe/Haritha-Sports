89819446953ec1bbe8434af62a3f6f05e849443e
Thimali1111 <thimalikaushalya1998@gmail.com>
2025-03-13 12:00:00 +0530

    remove product

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -59,5 +59,17 @@ const listProducts = async (req, res) => {
     }
 }
 
+// function for removing product
+const removeProduct = async (req, res) => {
+    try {
+        
+        await productModel.findByIdAndDelete(req.body.id)
+        res.json({success:true,message:"Product Removed"})
+
+    } catch (error) {
+        console.log(error)
+        res.json({ success: false, message: error.message })
+    }
+}
 
 export { listProducts, addProduct, removeProduct, singleProduct }
