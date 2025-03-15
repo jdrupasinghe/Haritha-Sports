b73688c1e7f774602fbfba6a0162ce6c47753ba3
Shahani123 <shahani.fazal3@gmail.com>
2025-03-15 12:00:00 +0530

    cart total

--- frontend/src/components/CartTotal.jsx
+++ frontend/src/components/CartTotal.jsx
@@ -4,8 +4,7 @@ import Title from './Title';
 
 const CartTotal = () => {
 
-    const {currency,delivery_fee,getCartAmount} = useContext(ShopContext);
-
+    
   return (
     <div className='w-full'>
       <div className='text-2xl'>
