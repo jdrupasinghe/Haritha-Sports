312b44d7dab2c7ebc16c08202b1b0de740fd83d5
Thimali1111 <thimalikaushalya1998@gmail.com>
2025-03-19 12:00:00 +0530

    update product item

--- frontend/src/components/ProductItem.jsx
+++ frontend/src/components/ProductItem.jsx
@@ -4,7 +4,7 @@ import {Link} from 'react-router-dom'
 
 const ProductItem = ({id,image,name,price}) => {
     
-    const {currency} = useContext(ShopContext);
+    
 
   return (
     <Link onClick={()=>scrollTo(0,0)} className='text-gray-700 cursor-pointer' to={`/product/${id}`}>
