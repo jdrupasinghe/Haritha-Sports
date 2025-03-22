1e21ea7f62e15b86cfd1a35f9b8d5cc2c0323d87
Shahani123 <shahani.fazal3@gmail.com>
2025-03-22 12:00:00 +0530

    update cart routes

--- backend/routes/cartRoute.js
+++ backend/routes/cartRoute.js
@@ -2,6 +2,8 @@ import express from 'express'
 import { addToCart, getUserCart, updateCart } from '../controllers/cartController.js'
 import authUser from '../middleware/auth.js'
 
+const cartRouter = express.Router()
+
 
 
 export default cartRouter
